2575e7c060eab0a1a897a1ce2ef74f18d25eacef
dimweb <dmitrymail_k@mail.ru>
2018-07-07 22:15:52 +0300

    React slider day 2(настроил компоненты)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
 class App extends Component {
   render() {
     return (
-      <h1>Hello dude!</h1>
+      <h1>Hello Dude!</h1>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
