898fb668c7bc907c500d863fe92bd621b3010089
dimweb <dmitrymail_k@mail.ru>
2018-07-08 00:41:50 +0300

    React slider day 2 (final)

--- src/App.js
+++ src/App.js
@@ -3,14 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+
+import Header from './components/Header';
+import Main from './components/Main';
+import Menu from './components/Menu';
+
+const url = process.env.PUBLIC_URL+'/img/bg.png';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+    height: 89px;
+    padding-top: 20px;
+` 
+
+const MainWrapper = styled.div`
+    height: 600px;
+    padding-top: 130px;
+    background: url(${url}) no-repeat;
+    background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello Dude!</h1>
+              <div className="App">
+              <HeaderWrapper>
+                <Grid>
+                  <Header/>
+                </Grid>
+              </HeaderWrapper>
+
+              <MenuWrapper>
+                <Grid>
+                  <Menu />
+                </Grid>  
+              </MenuWrapper>  
+
+              <MainWrapper>
+                <Grid>
+                  <Main />
+                </Grid>
+              </MainWrapper>
+              </div>
     );
   }
 }
